8d64a6f72f3bdeb5e59b1c6e7b338f0605424d7b
Carlos Cortés Sánchez <ccortes@gmv.com>
2018-02-28 16:02:44 +0100

    Test score is computed

--- BullsEyeTests/BullsEyeTests.swift
+++ BullsEyeTests/BullsEyeTests.swift
@@ -8,28 +8,29 @@
 
 import XCTest
 
+@testable import BullsEye
+
 class BullsEyeTests: XCTestCase {
+    
+    var gameUnderTest: BullsEyeGame!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        gameUnderTest = BullsEyeGame()
+        gameUnderTest.startNewGame()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        gameUnderTest = nil
+        
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // XCTAssert to test model
+    func testScoreIsComputed() {
+        let guess = gameUnderTest.targetValue + 5
+        _ = gameUnderTest.check(guess: guess)
+        XCTAssertEqual(gameUnderTest.scoreRound, 95, "Score computed from guess is wrong")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
